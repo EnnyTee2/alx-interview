04c0116986668365c74122723d79e6c67991d1f1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 21:24:45 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
-    a = []
-    if (n <= 0):
-        a.append([1])
-    elif (n == 1):
-        a.append([n])
-    else:
-        for i in range()
-    return a
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n:
+        Returns an empty list if n <= 0
+        assumes n will be always an integer
+    """
+    
+    master = []
+    temp = []
+    count = 0
+    
+    while count <= n:
+        if count == 0:
+            temp = [1]
+            master.append(temp)
+            
+        elif count == 1:
+            temp = [1, 1]
+            master.append(temp)
+        else:
+            outer = count-1
+            length = len(master[outer])
+            temp = []
+            for i in range(length+1):
+                if (i == 0 or i == length):
+                    temp.append(1)
+                if (i <= length-2):
+                    temp.append(master[outer][i] + master[outer][i + 1])
+
+            master.append(temp)
+        count += 1
+    return master
