4064b60a4800f4c776440a4d880d18e4f7e86eae
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 19:52:47 +0100

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    a = []
+    if (n <= 0):
+        a.append([1])
+    elif (n == 1):
+        a.append([n])
+    else:
+        for i in range()
+    return a
