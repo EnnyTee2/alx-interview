93a3213173a10e31e7f26b391a6c39a969abc12a
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-31 00:41:01 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,34 +4,34 @@
 
 def pascal_triangle(n):
     """
-    returns a list of lists of integers representing the Pascal triangle of n:
+    returns a list of lists of integers representing the Pascal triangle (PT) of n:
         Returns an empty list if n <= 0
         assumes n will be always an integer
     """
-    master = []
-    count = 0
+    master = [] # list of lists to hold the entire pascal triangle
+    count = 0 # counter variable
 
     if (n <= 0):
         return master
 
     while count < n:
-        if count == 0:
-            temp = [1]
+        if count == 0: # Step 1 of pascals triangle
+            temp = [1] # temporary list for hold the tem
             master.append(temp)
-
-        elif count == 1:
+        elif count == 1: # Step 2 of pascals triangle
             temp = [1, 1]
             master.append(temp)
-        else:
-            outer = count-1
-            length = len(master[outer])
-            temp = []
-            for i in range(length+1):
-                if (i == 0 or i == length):
-                    temp.append(1)
-                if (i <= length-2):
-                    temp.append(master[outer][i] + master[outer][i + 1])
+        else: # other higher steps
+            outer = count - 1 # return index of previous step
+            length = len(master[outer]) # length of preceding step of PT
+            temp = [] # empty the temporary holding list
+            
+            for i in range(length + 1): # looping for the next step of PT  
+                if (i == 0 or i == length): # if index is first or last
+                    temp.append(1) # insert the left and right bounding 1s
+                if (i <= length - 2): # Get the next PT set from the previous set
+                    temp.append(master[outer][i] + master[outer][i + 1]) # algo
 
-            master.append(temp)
-        count += 1
+            master.append(temp) # append the current list to the main Triangle list.
+        count += 1 # increment the counter
     return master
