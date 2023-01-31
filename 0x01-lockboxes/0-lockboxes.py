562842ef8a6434187633da54dd55727ec3f9be00
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-31 11:54:25 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,29 +5,29 @@
 def canUnlockAll(boxes):
     """ This function checks if all the boxes can be unlocked """
     
-    box_len = len(boxes) # get total number of boxes
-    keys = boxes[0] # initialised list to store availabe keys
-    box_list = {} # dictionary to keep track of each box's unlock status
-    count = 0 # unlock cycle counter
-    pending = 0 # tracker for unopened boxes
+    box_len = len(boxes)  # get total number of boxes
+    keys = boxes[0]  # initialised list to store availabe keys
+    box_list = {}  # dictionary to keep track of each box's unlock status
+    count = 0  # unlock cycle counter
+    pending = 0  # tracker for unopened boxes
 
     while count < box_len - pending:
-        x = 1 # initialise the box index for the next cycle
+        x = 1  # initialise the box index for the next cycle
         while x < box_len:
             if x in keys:
-                box_list[f'{x}'] = True # set box status
+                box_list[f'{x}'] = True  # set box status
                 for key in boxes[x]:
-                    if key not in keys: # if same key has not been added earlier
+                    if key not in keys:  # if same key has not been added earlier
                         keys.append(key)
             else:
-                box_list[f'{x}'] = False # set box status
+                box_list[f'{x}'] = False  # set box status
                 pending += 1
-            x += 1 # increment the index
-        count += 1 # increment the unlock cycle counter
-        if pending == 0: # if all boxes get unlocked after first cycle
+            x += 1  # increment the index
+        count += 1  # increment the unlock cycle counter
+        if pending == 0:  # if all boxes get unlocked after first cycle
             break
   
-    for status in box_list.values(): # Checking the box status after running all unlock attempts
+    for status in box_list.values():  # Checking the box status after running all unlock attempts
         if status == False:
-            return False # One ore more boxes can not be opened
-    return True # All the boxes can be opened
+            return False  # One ore more boxes can not be opened
+    return True  # All the boxes can be opened
