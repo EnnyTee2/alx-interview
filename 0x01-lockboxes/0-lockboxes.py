0bb2cb1725dc5367d29bfb931f382e98d8f72f0d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-08 00:51:45 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,25 +9,30 @@ def canUnlockAll(boxes):
         True: all boxes can be opened
         False: not all boxes can be opened
     """
-    length = len(boxes)
-    keys = set()
-    opened_boxes = []
-    i = 0
+    box_len = len(boxes)  # get total number of boxes
+    keys = boxes[0][:]  # initialised list to store availabe keys
+    box_list = {}  # dictionary to keep track of each box's unlock status
+    count = 0  # unlock cycle counter
+    pending = 0  # tracker for unopened boxes
 
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
-        for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
+    while (count < (box_len - pending)):
+        x = 1  # initialise the box index for the next cycle
+        while (x < box_len):
+            if x in keys:
+                box_list[f'{x}'] = True  # set box status
+                for key in boxes[x]:
+                    if key not in keys:  # if same key was not added earlier
+                        keys.append(key)
+            else:
+                box_list[f'{x}'] = False  # set box status
+                pending += 1
+            x += 1  # increment the index
+        count += 1  # increment the unlock cycle counter
+        if pending == 0:  # if all boxes get unlocked after first cycle
             break
 
-    for i in range(length):
-        if i not in opened_boxes and i != 0:
-            return False
-    return True
+# Checking the box status after running all unlock attempts
+    for status in box_list.values():
+        if status is False:
+            return False  # One ore more boxes can not be opened
+    return True  # All the boxes can be opened
