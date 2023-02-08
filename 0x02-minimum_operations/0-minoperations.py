730b4b79bcad6ffc5e523687004e89601ac9c93f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-08 17:54:48 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''ALX Minimum Operatiions Interview Question'''
+
+
+from typing import List
+
+
+global factors
+factors = (2,3,5,7)
+
+def lcm_gen(num):
+    '''Generates the LCMs for a given number'''
+    for factor in factors:
+        if (num % factor) == 0:
+            yield factor
+            yield from lcm_gen(num // factor) # recursive part of generator
+            break
+        else:
+            if (factor == 7) and (num != 1):
+                yield num
+
+def lcm(num):
+    '''Combines the yields from lcm generator'''
+    if num <= 1:
+        return 0
+    return list(lcm_gen(num)) # combine the yields from lcm generator
+
+def minOperations(n):
+    '''Returns the fewest number of operations'''
+    return sum(lcm(n))
+    
