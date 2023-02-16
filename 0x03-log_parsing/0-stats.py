0b7fab3f81d3bb72e719d8f38a1640fb3a4bcd86
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-16 20:34:31 +0100

    Create 0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Log Parsing """
+
+import sys
+import re
+
+
+match = '.*\..*\..*\..*\s\-\s\[.*?]\s".*"\s(200|301|400|401|403|404|405|500)\s(\d*)'
+line_count = 0
+total_size = 0
+status_code = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_count = dict([(code, 0) for code in status_code])
+
+def initialize():
+    line_count = 0
+    total_size = 0
+    for code in status_code:
+        status_count[code] = 0
+    return
+
+def status_writer():
+    for line in sys.stdin:
+        if line_count == 10:
+            print(f"File size: {total_size}")
+            for status_code,number in status_count.items():
+                print(f"{status_code}: {number}")
+            initialize()
+
+        capture = re.findall(match, line)
+        try:
+            total_size += int(capture[0][1])
+            status[capture[0][0]] += 1
+        except IndexError:
+            line_count += 1
+            continue
+
+        line_count += 1
+    
+try:
+    while True:
+        status_writer()
+
+except KeyboardInterrupt:
+    initialize()
+    status_writer()
