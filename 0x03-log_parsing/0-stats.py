01550422cd40d14747e5eb2d7d3dc6fadddbd28b
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-16 20:50:52 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,10 +4,11 @@
 import sys
 import re
 
-
-match = '.*\..*\..*\..*\s\-\s\[.*?]\s".*"\s(200|301|400|401|403|404|405|500)\s(\d*)'
 global line_count
 global total_size
+
+
+match = '.*\..*\..*\..*\s\-\s\[.*?]\s".*"\s(200|301|400|401|403|404|405|500)\s(\d*)'
 
 line_count = 0
 total_size = 0
