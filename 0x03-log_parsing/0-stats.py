b4908005b1d030575e395a3ba0fa4916e1167385
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-16 22:14:09 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,51 +9,32 @@ match = r'.*\..*\..*\..*\s\-\s\[.*?]\s".*"\s(200|301|400|401|403|404|405|500)\s(
 
 line_count = 0
 total_size = 0
-status_code = ['200', '301', '400', '401', '403', '404', '405', '500']
-status_count = dict([(code, 0) for code in status_code])
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_count = dict([(code, 0) for code in status_codes])
 
 
-def initialize():
-    """ Initializes the status fields """
-    global line_count
-    global total_size
-
-    line_count = 0
-    total_size = 0
-    for code in status_code:
-        status_count[code] = 0
-    return
-
-
-def status_writer():
-    """ Extracts the data fields from the stream
-        and prints the status log
-    """
-    global line_count
-    global total_size
-
-    try:
-        for line in sys.stdin:
-            if line_count % 10 == 0 and total_size != 0:
-                print(f"File size: {total_size}")
-                for status_code, number in status_count.items():
-                    if number != 0:
-                        print(f"{status_code}: {number}")
-                # initialize()
-
-            capture = re.findall(match, line)
-            try:
-                total_size += int(capture[0][1])
-                status_count[capture[0][0]] += 1
-            except IndexError:
-                line_count += 1
-                continue
-
+try:
+    for line in sys.stdin:
+        capture = re.findall(match, line)
+        try:
+            total_size += int(capture[0][1])
+            status_count[capture[0][0]] += 1
+        except IndexError:
             line_count += 1
+            continue
+        line_count += 1
+            
+        if line_count % 10 == 0 and total_size != 0:
+            print(f"File size: {total_size}")
+            for status_code, number in status_count.items():
+                if number != 0:
+                    print(f"{status_code}: {number}")
 
-    except KeyboardInterrupt:
-        status_writer()
+except KeyboardInterrupt:
+    pass
 
-
-status_writer()
-
+finally:
+    print(f"File size: {total_size}")
+    for status_code, number in status_count.items():
+        if number != 0:
+            print(f"{status_code}: {number}")
