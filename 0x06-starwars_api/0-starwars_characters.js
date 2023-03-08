6b97380dc1839d0dbddbdacd086eb032d49470b9
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-08 23:45:39 +0100

    Create 0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+
+let url = 'https://swapi-api.alx-tools.com/api/films/';
+
+let arg = process.argv[2]
+
+const path = url + arg + '/'
+
+request(path, (error, response, body) => {
+  // Printing the error if occurred
+    if(error) console.log(error)
+   
+    // Printing status code
+    console.log(response.statusCode);
+     
+    // Printing body
+    for (const idx of body) {
+      if (idx == 'characters') {
+        foreach (const a of idx) {
+          request(a, (error, response, body) => {
+            console.log(body)
+          });
+        }
+      }
+    }
+});
