4238ea009e4082df22d47a1d7c9a9b924074484f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-20 05:28:07 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
+'''rotate 2D matrix'''
+
 
 def rotate_2d_matrix(matrix):
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing '''
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
+
+
+"""def rotate_2d_matrix(matrix):
     """ Rotates a n x n matrix by 90 degrees """
      import copy
     matcopy = copy.deepcopy(matrix)
@@ -15,3 +39,4 @@ def rotate_2d_matrix(matrix):
         current += 1
         fwd = 0
         revrs = len(matrix) - 1
+"""
