338449e6e691758a52bc2890918595e8fbfb199c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-25 03:24:52 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,37 @@ Change comes from within
 
 
 def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any
+        number of coins you have, return -1
+    coins is a list of the values of the coins in your possession
+    """
+    if total <= 0:
+        return 0
+    coins = sorted(coins, reverse=True)
+    maxi = coins[0]
+    x = 0
+    coins.pop(0)
+    rem = total % maxi
+    if rem == 0:
+        return total / maxi
+    quot = total // maxi
+    while x < len(coins):
+        remc = rem % coins[x]
+        if remc == 0:
+            return quot + (rem / coins[x])
+        if x == len(coins) - 1:
+            return -1
+        quotc = rem // coins[x]
+        rem = remc
+        quot += quotc
+        x += 1
+    return quot
+
+
+'''def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     amount total when given a pile of coins of different values.
     """
@@ -24,34 +55,4 @@ def makeChange(coins, total):
         else:
             coin_idx += 1
     return coins_count
-
-
-'''def makeChange(coins, total):
-    """
-    Return: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any
-        number of coins you have, return -1
-    coins is a list of the values of the coins in your possession
-    """
-    if total <= 0:
-        return 0
-    coins = sorted(coins, reverse=True)
-    maxi = coins[1]
-    x = 0
-    coins.pop(0)
-    rem = total % maxi
-    if rem == 0:
-        return total / maxi
-    quot = total // maxi
-    while x < len(coins):
-        remc = rem % coins[x]
-        if remc == 0:
-            return quot + (rem // coins[x])
-        if x == 0:
-            return -1
-        quotc = rem // coins[x]
-        rem = remc
-        quot += quotc
-        x += 1
-    return quot'''
+'''
