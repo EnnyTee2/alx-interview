c21c2b8b72144e53e44311b87119620a9d0d3974
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-31 05:25:10 +0100

    Create 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid):
+    '''gets the perimeter of island in grid
+       Returns: int (perimeter)
+    '''
+    if grid == [] or grid == [[]]:
+        return 0
+    leng = len(grid)
+    lenc = len(grid[0])
+    perimeter = 0
+
+    for x in range(1, leng):
+        for i in range(1, lenc):
+            if grid[x][i] == 1:
+                if grid[x][i-1] == 0:
+                    perimeter += 1
+                if grid[x][i+1] == 0:
+                    perimeter += 1
+                if grid[x-1][i] == 0:
+                    perimeter += 1
+                if grid[x+1][i] == 0:
+                    perimeter += 1
+    return perimeter
