52b1675aafff943a9d28b89c6492864653e104d5
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-31 06:01:22 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -58,7 +58,7 @@ def island_perimeter(grid):
                     # bottom side
                     if grid[lst_idx + 1][land_idx] == 0:
                         counter += 1
-    
+
     return counter
 
 
