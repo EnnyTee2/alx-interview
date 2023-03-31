b0b84992e8cf28026a9ded87e5bbde5475f45074
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-03-31 05:50:26 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,46 @@
 
 
 def island_perimeter(grid):
+    ""Return the perimeter
+    of the island described in grid:""
+    perimeter = 0
+    glen = len(grid)
+    zlen = len(grid[0])
+
+    for i in range(0, glen):
+        if i == 0 or i == glen - 1:  # top or bottom border
+            for j in range(0, zlen):
+                if grid[i][j] == 1:  # land found on border
+                    if grid[i][j+1] == 0 && grid[i][j-1] == 0:
+                        perimeter += 3
+                    while j > 0 && j < zlen -1:
+                        if grid[i][j-1] == 0 && grid[i][j+1] == 1:
+                            perimeter += 2
+                        if grid[i][j-1] == 1 && grid[i][j+1] == 1:
+                            perimeter += 1
+                    
+                    
+        else:
+            for j in range(0, zlen):
+                if j == 0 or j == zlen - 1:  # left or right border
+                    if grid[i][j] == 1:  # land found on border
+                        
+                else:
+                    if grid[i][j] == 1:  # land found in cell
+                        # The if blocks below detects
+                        # if the cell is surrounded by water
+                        if grid[i][j+1] == 0:
+                            perimeter += 1
+                        if grid[i][j-1] == 0:
+                            perimeter += 1
+                        if grid[i+1][j] == 0:
+                            perimeter += 1
+                        if grid[i-1][j] == 0:
+                            perimeter += 1
+    return perimeter
+
+
+"""def island_perimeter(grid):
     '''gets the perimeter of island in grid
        Returns: int (perimeter)
     '''
@@ -27,3 +67,4 @@ def island_perimeter(grid):
     except Exception as e:
         return 0
     return perimeter
+"""
