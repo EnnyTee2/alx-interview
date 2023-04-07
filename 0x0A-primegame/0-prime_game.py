6fafa32d9f038d84b7a0ed30c2ea39ba0c838177
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-04-07 02:40:42 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,7 +4,6 @@
 
 def isPrime(num):
     """ checks if a number is prime """
-
     status = 1
     if num == 1:
         return False
@@ -23,12 +22,12 @@ def isPrime(num):
 
 def isWinner(x, nums):
     """
+    Args:
         x: number of rounds
         nums: array of n
-        Returns: name of the winning player
+    Return: name of the winning player
         n and x are assumed to be <= 10000
     """
-
     if x is None or nums is None or x == 0 or nums == []:
         return None
     Ben = 0
