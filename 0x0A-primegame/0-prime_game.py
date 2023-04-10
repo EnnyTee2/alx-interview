be4faa41360169b4660bdb0bb48793db68b1b6a5
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-04-10 03:43:03 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -27,7 +27,7 @@ def isWinner(x, nums):
     Ben = 0
     Maria = 0
     count = 0
-    while count < len(nums):
+    while count < x:
         set_prime = [z for z in range(1, nums[count]+1) if isPrime(z)]
         prime_count = len(set_prime)
         if prime_count % 2 == 0:
