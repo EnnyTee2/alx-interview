95d5c07d16faf62fc39e05a9ea7412f006e2f741
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-04-10 03:07:03 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,14 +3,10 @@
 
 
 def isPrime(num):
-    """ checks if a number is prime """
-    temp = 0
-	if num == 1 or num == 0:
-		return False
-	for i in range(2, int(num/2) + 1):
-		if num % i == 0:
-			return False
-	return True
+    for i in range(2, (num//2) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
 def isWinner(x, nums):
